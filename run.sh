44a8c6db2c94cc1840cf1d991ff0011f40e858f9
Desmond Blue Manthy <blue@Desmonds-MacBook-Pro.local>
2018-11-08 14:07:24 -0700

    try2

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+npm install body-parser -save
+npm install cookie-parser -save
+npm install method-override -save
+npm install ejs -save
+npm install express -save
+npm install express-flash -save
+npm install express-session -save
+npm install express-validator -save
+npm install pg-promise -save
+
